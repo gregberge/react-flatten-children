1e5f38171cd6fd754de6c13d19a185e165720562
Kirill Konshin <kirill-konshin@users.noreply.github.com>
2020-04-19 11:31:45 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function flattenChildren(children, all) {
   return React.Children.toArray(children).reduce((flatChildren, child) => {
     if (child.type === React.Fragment || (all && child && child.props && child.props.children)) {
-      return flatChildren.concat(flattenChildren(child.props.children))
+      return flatChildren.concat(flattenChildren(child.props.children, all))
     }
     flatChildren.push(child)
     return flatChildren
